657ff6f7ed667b409b8c93ef49414942fe7b358d
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2026-09-01 18:21:03 -0300

    Components
    Updated:
     - Remade components for typescript

---
+++ WebSite/WebSiteReactVite/WebSite/src/components/shared/Banner.tsx
@@ -0,0 +1,18 @@
+//#region imports
+import React from "react";
+
+import "./Banner.css"
+
+export default Banner;
+//#endregion
+
+//#region JSX
+function Banner()
+{
+  return(
+    <div className="title">
+      <h1>SkillHub</h1>
+    </div>
+  );
+}
+//#endregion
