7ef79e28f4aecde7a4f1aaf0cf73ba30490ef602
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2026-09-01 18:20:11 -0300

    Entry Point on main
    Added:
     - Included AuthProvider and BrowseRouter on main

--- WebSite/WebSiteReactVite/WebSite/src/main.tsx
+++ WebSite/WebSiteReactVite/WebSite/src/main.tsx
@@ -1,30 +1,30 @@
-import { BrowserRouter } from 'react-router'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import { createRoot } from "react-dom/client";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import "./main.css";
 
-import { PrivateRoute } from './router/PrivateRoute';
+import { AuthProvider } from './router/AuthContext';
+import { PrivateRoute } from "./router/PrivateRoute";
 
-import Home from './pages/HomePage/HomePage';
-import Account from './pages/AccountPage/AccountPage';
-import Course from './pages/CoursePage/CoursePage';
-import Login from './pages/LoginPage/LoginPage';
-import RecoverPassword from './pages/RecoverPasswordPage/RecoverPasswordPage';
+import Home from "./pages/HomePage/HomePage";
+import Account from "./pages/AccountPage/AccountPage";
+import Course from "./pages/CoursePage/CoursePage";
+import Login from "./pages/LoginPage/LoginPage";
+import RecoverPassword from "./pages/RecoverPasswordPage/RecoverPasswordPage";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />s
-          <Route path="/login" element={<Login />} />
-          <Route path="/recover-password" element={<RecoverPassword />} />
-          <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
-          <Route path="/course" element={<PrivateRoute><Course /></PrivateRoute>} />
-        </Routes>
-      </div>
-    </Router>
-  </BrowserRouter>,
-)
+createRoot(document.getElementById("root")!).render(
+    <AuthProvider>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/recover-password" element={<RecoverPassword />} />
+                    <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
+                    <Route path="/course" element={<PrivateRoute><Course /></PrivateRoute>} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    </AuthProvider>
+);
