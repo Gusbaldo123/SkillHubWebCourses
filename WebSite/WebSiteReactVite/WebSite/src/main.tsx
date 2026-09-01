6118e156e3a82bed1095417dbb4c99aa55926502
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2026-09-01 17:07:04 -0300

    New entry point
    Added:
     - Now main.ts has the router

--- WebSite/WebSiteReactVite/WebSite/src/main.tsx
+++ WebSite/WebSiteReactVite/WebSite/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+
+import { PrivateRoute } from './router/PrivateRoute';
+
+import Home from './pages/HomePage/HomePage';
+import Account from './pages/AccountPage/AccountPage';
+import Course from './pages/CoursePage/CoursePage';
+import Login from './pages/LoginPage/LoginPage';
+import RecoverPassword from './pages/RecoverPasswordPage/RecoverPasswordPage';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />s
+          <Route path="/login" element={<Login />} />
+          <Route path="/recover-password" element={<RecoverPassword />} />
+          <Route path="/account" element={<PrivateRoute><Account /></PrivateRoute>} />
+          <Route path="/course" element={<PrivateRoute><Course /></PrivateRoute>} />
+        </Routes>
+      </div>
+    </Router>
+  </BrowserRouter>,
 )
