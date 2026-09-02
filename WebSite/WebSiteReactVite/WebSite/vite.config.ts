e9df35e10a77ba38f769669ee716ab0062fa93f4
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2026-09-01 22:13:04 -0300

    Docker for frontend
    Added:
     - Dockerized for dev builds

--- WebSite/WebSiteReactVite/WebSite/vite.config.ts
+++ WebSite/WebSiteReactVite/WebSite/vite.config.ts
@@ -2,10 +2,15 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// vite.config.js
+
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  server: {
+    host: true, // Needed for Docker tracking
+    port: 5173, // Default Vite port
+    watch: {
+      usePolling: true, // Fixes hot-reload issues on Windows/WSL
+    },
+  },
 })
