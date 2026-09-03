7d1323836a89ab51da3e85edaf137f92eb616b88
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2026-09-02 23:08:36 -0300

    Delete old project
    Deleted:
     - Old create-react-app project
     - All the old things was already migrated
    
    TODO:
     - Implementation bugs

--- WebSite/website/src/components/shared/Banner.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Banner from './Banner';
-
-test('renderiza o título "SkillHub"', () => {
-  render(<Banner />);
-  const title = screen.getByText(/SkillHub/i);
-  expect(title).toBeInTheDocument();
-});
